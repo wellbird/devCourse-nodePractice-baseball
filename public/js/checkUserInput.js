4c4763b6567bb98e5aedc2e3fc4b739160d6d191
Jaewon <wellbird@naver.com>
2024-11-21 23:54:56 +0900

    feat : 유저 입력 유효성 검사 구현

---
+++ public/js/checkUserInput.js
@@ -0,0 +1,19 @@
+function checkUserInput(event) {
+  const userInput = document.getElementById('userInput').value;
+
+  if (!/^[0-9]+$/.test(userInput)) {
+    alert('숫자만 입력할 수 있습니다.');
+    event.preventDefault();
+    return false;
+  }
+
+  if (userInput.length !== 3) {
+    alert('3자리 숫자를 입력해야 합니다.');
+    event.preventDefault();
+    return false;
+  }
+
+  return true;
+}
+
+module.exports = checkUserInput;
